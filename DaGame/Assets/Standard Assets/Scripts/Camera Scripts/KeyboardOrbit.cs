85c4e705246f496ef73517ad70688a7936c1313d
Numppa <joel.nummelin@elisanet.fi>
2013-11-02 23:33:13 +0200

    health bar

--- DaGame/Assets/Standard Assets/Scripts/Camera Scripts/KeyboardOrbit.cs
+++ DaGame/Assets/Standard Assets/Scripts/Camera Scripts/KeyboardOrbit.cs
@@ -3,50 +3,60 @@ using System.Collections;
 
 public class KeyboardOrbit : MonoBehaviour {
 	public Transform target;
-	
-	private float zoomSpeed = 3.0f;
-	private float minZoom = 20.0f;
-	private float maxZoom = 1000.0f;
-	private float cameraSpeed = 3.0f;
-	
-	private float z = 100.0f;
-	private float x = 1.0f;
-	private float y = 1.0f;
-	private bool eDown = false;
-	private bool qDown = false;
+	private float zoomSpeed;
+	private float minZoom;
+	private float maxZoom;
+	private float cameraSpeed;
+	private float z;
+	private float x;
+	private float y;
+	private bool eDown;
+	private bool qDown;
 	// Use this for initialization
-	void Start () {
+	void Start (){
+		zoomSpeed = 3.0f;
+		minZoom = 20.0f;
+		maxZoom = 1000.0f;
+		cameraSpeed = 3.0f;
+		
+		z = 100.0f;
+		x = 1.0f;
+		y = 1.0f;
+		eDown = false;
+		qDown = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (target)
-		x -= Input.GetAxis("Horizontal") * cameraSpeed;
-		y += Input.GetAxis("Vertical") * cameraSpeed;
+		x -= Input.GetAxis ("Horizontal") * cameraSpeed;
+		y += Input.GetAxis ("Vertical") * cameraSpeed;
 		
-		if (Input.GetKeyUp(KeyCode.Q)){
+		updateZoom();
+		
+		Quaternion rotation = Quaternion.Euler (y, x, 0);
+		
+		transform.rotation = rotation;
+		transform.position = rotation * new Vector3 (0, 0, -z) + target.position;
+	}
+
+	void updateZoom () {
+		if (Input.GetKeyUp (KeyCode.Q)) {
 			qDown = false;
-		} else if (Input.GetKeyUp(KeyCode.E)){
+		} else if (Input.GetKeyUp (KeyCode.E)) {
 			eDown = false;
 		}
-		if (Input.GetKeyDown(KeyCode.Q) || qDown){
+		if (Input.GetKeyDown (KeyCode.Q) || qDown) {
 			qDown = true;
 			z += zoomSpeed;
-		} else if (Input.GetKeyDown(KeyCode.E) || eDown){
+		} else if (Input.GetKeyDown (KeyCode.E) || eDown) {
 			eDown = true;
 			z -= zoomSpeed;
 		}
 		
-		if (z < minZoom){
+		if (z < minZoom) {
 			z = minZoom;
-		} else if (z > maxZoom){
+		} else if (z > maxZoom) {
 			z = maxZoom;
 		}
-		
-		Quaternion rotation = Quaternion.Euler(y, x, 0);
-		
-		transform.rotation = rotation;
-		transform.position = rotation * new Vector3(0, 0,  -z) + target.position;
 	}
-	}
-
+}
---
+++ DaGame/Assets/Standard Assets/Scripts/General Scripts/BaseShipBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseShipBehaviour : MonoBehaviour {
+	public Transform target;
+	public float maxHealth;
+	public float repair;
+	
+	private float health;
+	
+	// Use this for initialization
+	void Start () {
+		maxHealth = 1000.0f;
+		repair = -0.0f;
+		health = maxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		health += repair;
+	}
+
+	void OnGUI(){
+		GUI.contentColor = Color.green;
+		GUI.Box(new Rect(10, 10, Screen.width / 2, 20),new GUIContent((int) health + " / " + (int) maxHealth));
+	}
+	
+}
