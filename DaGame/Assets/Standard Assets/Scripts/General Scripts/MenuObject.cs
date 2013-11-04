41ecc8076fcbfaa1dd74ba6d6f2272cc2f407c0a
Tahantos <jokkend@gmail.comgit config --global user.email jokkend@gmail.com>
2013-11-04 20:46:38 +0200

    options menu lisatty ja toiminnallisuus siihen

--- DaGame/Assets/Standard Assets/Scripts/General Scripts/MenuObject.cs
+++ DaGame/Assets/Standard Assets/Scripts/General Scripts/MenuObject.cs
@@ -4,36 +4,59 @@ using System.Collections;
 
 
 
-
 public class MenuObject : MonoBehaviour {
 
 	// Use this for initialization
 
 
 
-  public bool isQuit = false;
-
-  void OnMouseEnter() {
-
-    renderer.material.color = Color.blue;
-
-  }
-
-
-  void OnMouseExit() {
-
-    renderer.material.color = Color.white;
   
-  }
+	public int level = 0;
 
-
-  void OnMouseDown() {
-    if(isQuit) {
-      Application.Quit();
-    } else {
-      Application.LoadLevel(1);
-    }
   
-  }
+	void OnMouseEnter() {
+
+    	renderer.material.color = Color.blue;
+
+  
+	}
+
+
+
+	void OnMouseExit() {
+
+   
+		renderer.material.color = Color.white;
+  
+  
+	}
+
+
+
+	void OnMouseDown() {
+		
+    
+		if(level==99) {
+      
+			Application.Quit();
+    
+		} else if (level == 50) {
+			    TextMesh t = (TextMesh)gameObject.GetComponent(typeof(TextMesh));
+			
+			if (t.text == "Sound: Off") {
+				t.text = "Sound: On";
+				AudioListener.pause = false;
+			} else {
+				t.text = "Sound: Off";
+				AudioListener.pause = true;
+			}
+		} else {
+      
+			Application.LoadLevel(level);
+    
+		}
+  
+  
+	}
 
 }
