94ae19c3d93401e15b3e9da7773eae6db7c072e5
Tahantos <jokkend@gmail.comgit config --global user.email jokkend@gmail.com>
2013-11-05 20:08:01 +0200

    selectaus toimii nyt ja sivuun ilmestyy boxi

--- DaGame/Assets/Standard Assets/Scripts/General Scripts/PlaneObject.cs
+++ DaGame/Assets/Standard Assets/Scripts/General Scripts/PlaneObject.cs
@@ -4,23 +4,52 @@ using System.Collections;
 public class PlaneObject : MonoBehaviour {
 	
 	private bool hasTurret = false;
-
-	void OnMouseEnter() {
-		if (!hasTurret) {	
-    		renderer.material.color = Color.green;
+	private bool selected = false;
+	public static PlaneObject selection;
+	
+	void Update() {
+		if(selection != this) {
+			selected = false;
 		}
+		if (selected == false && renderer.material.color == Color.blue) {
+			renderer.material.color = Color.white;	
+		}
+		
 	}
 
-
+	void OnMouseEnter() {
+		if (!hasTurret && !selected) {
+			
+    		renderer.material.color = Color.green;
+			
+		} 
+	}
 
 	void OnMouseExit() {
-   		if (!hasTurret) {
+   		if (!hasTurret && !selected) {
 			renderer.material.color = Color.white;
 		}
 	}
 	
 	void OnMouseDown() {
+		if (!selected && renderer.isVisible) {
+			renderer.material.color = Color.blue;
+			selected = true;
+			selection = this;
+		} else {
+			
+			renderer.material.color = Color.white;
+			selected = false;
+			
 		
+		}		
 		
+	}
+	
+	void OnGUI() {
+		if(selected) {
+			Rect rect = new Rect(Screen.width-220, 10, 180, Screen.height / 1.3f);
+			GUI.Box(rect, "Workshop\n"+this.gameObject.name);
+		}
 	}
 }
