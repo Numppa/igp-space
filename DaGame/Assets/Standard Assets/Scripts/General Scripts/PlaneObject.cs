dcd53389bc1b17b7b992ccf94e7ae4b84a101f31
Tahantos <jokkend@gmail.comgit config --global user.email jokkend@gmail.com>
2013-11-05 18:01:20 +0200

    planes and stuff with scripts now

---
+++ DaGame/Assets/Standard Assets/Scripts/General Scripts/PlaneObject.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaneObject : MonoBehaviour {
+	
+	private bool hasTurret = false;
+
+	void OnMouseEnter() {
+		if (!hasTurret) {	
+    		renderer.material.color = Color.green;
+		}
+	}
+
+
+
+	void OnMouseExit() {
+   		if (!hasTurret) {
+			renderer.material.color = Color.white;
+		}
+	}
+	
+	void OnMouseDown() {
+		
+		
+	}
+}
