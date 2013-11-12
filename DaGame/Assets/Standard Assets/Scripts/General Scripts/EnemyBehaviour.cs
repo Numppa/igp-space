3ccbc1669165ce3a9842847b045e2eef9e743189
Numppa <joel.nummelin@elisanet.fi>
2013-11-12 17:41:10 +0200

    blaa

---
+++ DaGame/Assets/Standard Assets/Scripts/General Scripts/EnemyBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehaviour : MonoBehaviour {
+	public int health;
+	public int radarVisibility;
+	
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (health <= 0){
+			Destroy(gameObject);
+		}
+	}
+	
+	void hit(int damage){
+		health -= damage;
+	}
+}
