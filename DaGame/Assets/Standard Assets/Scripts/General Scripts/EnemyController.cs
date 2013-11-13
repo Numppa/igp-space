fc34712bb22c6bda24ab8853ecd1b879dd2ffffa
aapomalk <aapo.malkamaki@helsinki.fi>
2013-11-13 22:10:06 +0200

    reititysta ja valaistusta

--- DaGame/Assets/Standard Assets/Scripts/General Scripts/EnemyController.cs
+++ DaGame/Assets/Standard Assets/Scripts/General Scripts/EnemyController.cs
@@ -3,25 +3,17 @@ using System.Collections;
 
 public class EnemyController : MonoBehaviour {
 	
-	public int timeBetweenEnemies;
 	public int timeBetweenWaves;
 	private int timer;
-	public GameObject[] wave0;
-	public GameObject[] wave1;
-	private int index = 0;
+	public GameObject[] waves;
 	private int wave = 0;
 	
 	// Use this for initialization
 	void Start () {
-		timer = timeBetweenEnemies;
-		for (int i = 0; i < wave1.Length; i++) {
-			if(wave1[i].activeSelf == true) {
-				wave1[i].SetActive (false);
-			}
-		}
-		for (int i = 0; i < wave0.Length; i++) {
-			if(wave0[i].activeSelf == true) {
-				wave0[i].SetActive (false);
+		timer = timeBetweenWaves;
+		foreach (GameObject enemy in waves) {
+			if(enemy.activeSelf == true) {
+				enemy.SetActive (false);
 			}
 		}
 	}
@@ -31,22 +23,8 @@ public class EnemyController : MonoBehaviour {
 		if (timer > 0) {
 			timer--;
 		} else {
-			if (wave == 0) {
-				activateEnemy(wave0);
-			} else if (wave == 1) {
-				activateEnemy(wave1);
-			}
-		}
-	}
-	
-	private void activateEnemy(GameObject[] waveNumber) {
-		if (waveNumber[index].activeSelf == false) {
-			waveNumber[index].SetActive (true);
-			if (index < wave1.Length -1) {
-				index++;
-				timer = timeBetweenEnemies;
-			} else {
-				index = 0;
+			waves[wave].SetActive (true);
+			if (waves.Length >= wave + 2) {
 				timer = timeBetweenWaves;
 				wave++;
 			}
