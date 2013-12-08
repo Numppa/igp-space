985c3c022ada98139132e545e41d210bb96eb1bd
aapomalk <aapo.malkamaki@helsinki.fi>
2013-12-08 22:04:43 +0200

    enemy explosionSound works

---
+++ DaGame/Assets/Standard Assets/Scripts/General Scripts/DestroyAfterHearing.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyAfterHearing : MonoBehaviour {
+	public AudioClip clip;
+	
+	// Use this for initialization
+	void Start () {
+		Destroy(gameObject, clip.length);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- DaGame/Assets/Standard Assets/Scripts/General Scripts/EnemyBehaviour.cs
+++ DaGame/Assets/Standard Assets/Scripts/General Scripts/EnemyBehaviour.cs
@@ -8,7 +8,7 @@ public class EnemyBehaviour : AbstractHitable {
 	public float maxFireDistance;
 	public GameObject explosion;
 	public int bounty;
-	public AudioClip explosionSound;
+	public GameObject soundObject;
 	
 	// Update is called once per frame
 	void Update () { 
@@ -30,8 +30,8 @@ public class EnemyBehaviour : AbstractHitable {
 		health -= damage;
 		if (health <= 0){
 			collider.enabled = false;
-			audio.PlayOneShot(explosionSound);
 			GameObject explo = (GameObject) Instantiate(explosion, gameObject.transform.position, gameObject.transform.rotation);
+			GameObject sound = (GameObject) Instantiate(soundObject, gameObject.transform.position, gameObject.transform.rotation);
 			Destroy(gameObject);
 			GameObject.FindWithTag("spehsShip").BroadcastMessage("ChangeResources", bounty);
 			Destroy(explo, 3);
